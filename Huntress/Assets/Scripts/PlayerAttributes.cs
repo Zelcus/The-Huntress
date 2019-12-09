4226ef8b3b88c64469ab6ffb7824028b0dbc396f
Zacharias <zelcus@hotmail.com>
2019-12-09 11:19:12 +0100

    Added health that decreases with 5 every second

---
+++ Huntress/Assets/Scripts/PlayerAttributes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+
+    public int startHealth = 100;
+
+    public int bonusHealth;
+
+    private int totalHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalHealth = startHealth + bonusHealth;
+        InvokeRepeating("DecreaseHealth", 1.0f, 1.0f);
+    }
+
+    void DecreaseHealth()
+    {
+        if (totalHealth > 0)
+        {
+            totalHealth -= 5;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(totalHealth);   
+    }
+}
